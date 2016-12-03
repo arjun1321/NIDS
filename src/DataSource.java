2a8512d60bef1e21f84a0f5dd8a1fc6b442e58f1
arjun1321 <guptaarjun152@gmail.com>
2016-12-03 12:52:53 +0530

    Initial Commit

---
+++ src/DataSource.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dewesh
+ */
+
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import java.io.File;
+
+public class DataSource {
+    public static Instances read(String filename) throws Exception {
+    ArffLoader loader = new ArffLoader();
+    loader.setSource(new File(filename));
+    Instances data = loader.getDataSet();
+    return data;
+  }
+}
---
+++ src/NB.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dewesh
+ */
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JTextArea;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.classifiers.bayes.NaiveBayesUpdateable;
+import weka.filters.unsupervised.attribute.Remove;
+import weka.classifiers.meta.FilteredClassifier;
+
+public class NB{
+    String train_path;
+    String test_path;
+    JTextArea jta1;
+    JTextArea jta2;
+
+    public void run(){
+        try{
+            Instances train= DataSource.read(train_path);
+            Instances test= DataSource.read(test_path);
+            train.setClassIndex(train.numAttributes()-1);
+            test.setClassIndex(test.numAttributes()-1);
+
+            if(!train.equalHeaders(test))
+                throw new IllegalArgumentException("datasets are not compatible..");
+
+            Remove rm=new Remove();
+            rm.setAttributeIndices("1");
+
+
+            NaiveBayesUpdateable nb= new NaiveBayesUpdateable();
+            train.setClassIndex(train.numAttributes()-1);
+            nb.buildClassifier(train);
+
+            int total_anamoly=0;
+            int total_instances=0;
+            int ana_np=0;
+            int n_ana_p=0;
+            int ana_p=0;
+            int cp=0;
+            int icp=0;
+
+
+            for(int i=0;i<test.numInstances();i++){
+                double pred = nb.classifyInstance(test.instance(i));
+                String a="anomaly";
+                String actual;
+                String predicted;
+                actual=test.classAttribute().value((int) test.instance(i).classValue());
+                predicted= test.classAttribute().value((int) pred);
+                jta1.append("ID: " + i);
+                jta1.append(", actual: "+ actual);
+                jta1.append(", predicted: "+ predicted);
+                jta1.append("\n-----------------------------------------------------------\n");
+                if(actual.equalsIgnoreCase(a))
+                    total_anamoly++;
+                if(actual.equalsIgnoreCase(predicted))
+                    cp++;
+                if(!actual.equalsIgnoreCase(predicted))
+                    icp++;
+                if(actual.equalsIgnoreCase(a)&&predicted.equalsIgnoreCase(a))
+                    ana_p++;
+                if((!actual.equalsIgnoreCase(a))&&predicted.equalsIgnoreCase(a))
+                    n_ana_p++;
+                if(actual.equalsIgnoreCase(a)&&(!predicted.equalsIgnoreCase(a)))
+                    ana_np++;
+                total_instances++;
+            }
+            double accuracy=(cp*100)/(cp+icp);
+            double recall=ana_p*100/(total_anamoly);
+            double precision= ana_p*100/(ana_p+n_ana_p);
+            jta2.append("\n total_instances : "+total_instances+"     total_anamoly : "+total_anamoly);
+            jta2.append("\n correct pred :"+cp+"     incorrect predictions : "+icp);
+            jta2.append("\n precision"+precision+"     recall : "+recall);
+            jta2.append("\n accuracy : "+accuracy);
+            //jta2.append("\n ana_p : "+ana_p+"  n_ana_p : "+n_ana_p+"  ana_np : "+ana_np);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void init(String trainpath, String testpath, JTextArea jta2,JTextArea jta3) {
+		// TODO Auto-generated method stub
+		this.train_path=trainpath;
+		this.test_path=testpath;
+		this.jta1=jta2;
+                this.jta2=jta3;
+    }
+}
